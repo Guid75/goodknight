8ccb81d89fa2bb42820f3fa14494fde07afcfcd6
Guillaume Denry <guillaume.denry@gmail.com>
2016-01-13 23:18:45 +0100

    text style becomes parametrable for textToDiv

--- main.elm
+++ main.elm
@@ -7,20 +7,18 @@ import Text
 import StartApp.Simple as StartApp
 import Cards exposing (..)
 
-
 main =
   StartApp.start { model = model, view = view, update = update }
-
 
 model = 0
 
 view address model =
   div []
-    (List.map textToDiv initialLandscapeDeck)
+    (List.map (textToDiv landscapeStyle) initialLandscapeDeck)
 
 -- transform anything into an HTML div with just its textual representation
-textToDiv : a -> Html.Html
-textToDiv a = div [landscapeStyle] [(text << toString) a]
+textToDiv : Html.Attribute -> a -> Html.Html
+textToDiv attr a = div [attr] [(text << toString) a]
 
 landscapeStyle : Html.Attribute
 landscapeStyle =
@@ -31,7 +29,6 @@ landscapeStyle =
   ]
 
 type Action = Increment | Decrement
-
 
 update action model =
   case action of
